b9eddc70d0506971c63e13822ab288d8f73dac64
Khontrol <kinghornmarketing@gmail.com>
2023-09-08 12:54:05 -0400

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -450,6 +450,28 @@ jQuery(document).ready(function($) {
 	stickControls();
 	wnd.on('resize', stickControls);
 
+	document.addEventListener("DOMContentLoaded", () => {
+		const slider = document.getElementById("slider");
+		const slides = document.querySelectorAll(".slide");
+		let currentSlide = 0;
+	  
+		document.getElementById("nextBtn").addEventListener("click", () => {
+		  currentSlide = (currentSlide + 1) % slides.length;
+		  slider.scroll({
+			left: slides[currentSlide].offsetLeft,
+			behavior: "smooth"
+		  });
+		});
+	  
+		document.getElementById("prevBtn").addEventListener("click", () => {
+		  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+		  slider.scroll({
+			left: slides[currentSlide].offsetLeft,
+			behavior: "smooth"
+		  });
+		});
+	  });
+
 
 	/* ----------------------------
 
@@ -513,3 +535,4 @@ jQuery(document).ready(function($) {
 	});
 
 });
+
